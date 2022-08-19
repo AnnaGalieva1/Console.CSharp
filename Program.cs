9697bccb7877050dd36af3099b7aa7594e0ca0b1
Anna Galieva <annet_forever08@mail.ru>
2022-08-19 23:04:26 +0300

    задача1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+Console.WriteLine("Введите число 1: ");
+int usernumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int usernumber2 = Convert.ToInt32(Console.ReadLine());
+int max = usernumber1;
+int min = usernumber2;
+if (usernumber1 > usernumber2)
+{
+    Console.WriteLine("Большее число: " + usernumber1);
+    Console.WriteLine("Меньшее число: " + usernumber2);
+}
+else
+{
+    Console.WriteLine("Большее число: " + usernumber2);
+    Console.WriteLine("Меньшее число: " + usernumber1);
+}
